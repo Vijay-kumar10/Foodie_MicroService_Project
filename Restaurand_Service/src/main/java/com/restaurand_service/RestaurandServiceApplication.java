5ace2eaca2822bc3220a7f46875e29970c6a6a49
Vijay-kumar10 <kumar888985@gmail.com>
2026-05-05 22:47:44 +0530

    Restaurent-Service Created

---
+++ Restaurand_Service/src/main/java/com/restaurand_service/RestaurandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restaurand_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurandServiceApplication.class, args);
+    }
+
+}
