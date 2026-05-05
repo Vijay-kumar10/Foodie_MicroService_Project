bab57191d05da10785a6db9ad73458d3456f69f1
Vijay-kumar10 <kumar888985@gmail.com>
2026-05-05 22:54:06 +0530

    Payment_And_Delivery_Service is created

---
+++ Payment_Delivery_Service/src/main/java/com/payment_delivery_service/PaymentDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payment_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentDeliveryServiceApplication.class, args);
+    }
+
+}
---
+++ Payment_Delivery_Service/src/test/java/com/payment_delivery_service/PaymentDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDeliveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
